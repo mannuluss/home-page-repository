e29dc523cd70e4ea3cbd8ea8285f3f24c132d74d
mannuluss <pipe_050401@hotmail.com>
2022-06-30 09:15:05 -0500

    https nestjs and enviroment

---
+++ Jenkinsfile
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Install dependence') {
+            steps {
+                sh 'npm install'
+            }
+        }
+        stage('Build Frontend') {
+            steps {
+                echo 'Building...'
+                sh 'cd frontend'
+                sh 'npm run build'
+            }
+        }
+        
+        stage('Build Backend') {
+            steps {
+                echo 'Building...'
+                sh 'cd backend'
+                sh 'npm run build'
+            }
+        }
+    }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.HTTPS_KEY || './secrets/private-key.pem'),
+    cert: fs.readFileSync(process.env.HTTPS_CERT || './secrets/public-certificate.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
   app.enableCors();
   var puerto = process.env.PORT || 3000;
   Logger.debug(`PORT : ${puerto}`);
